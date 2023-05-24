c5cd72fb3a38f289c8ceb083250d4c8a29a5c0d0
Razer Coding <134110859+cacheplays@users.noreply.github.com>
2023-05-23 19:10:52 -0600

    Add files via upload

---
+++ example code/basics/lesson2.py
@@ -0,0 +1,16 @@
+#print, print adds something to the console
+print("hello world!")
+#there are two different types of variables, one type is strings or str string are shown with quotations "" integers or int are numbers without decimals, math opperations can be done on these variables
+number = 5
+string = "hello"
+print(number)
+print(string)
+# there are two functions that you can use to convert string and integers one is int() another is str()
+#int and str
+number_str = "25"
+number_int = int(number_str)
+number_int += 5
+print(number_int)
+
+input = input("what is you're favorite color\n")
+print("\"cool!", input, "is my farvorite color too!\"")
